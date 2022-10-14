945036da58d457c13c22ed91e1e530112ff8c27c
Shivya <yadavshivangi366@gmail.com>
2022-10-14 13:03:07 +0530

    added

---
+++ loop_1.php
@@ -0,0 +1,23 @@
+<?php
+
+//for loop
+for($a=0;$a<=5;$a++)
+{
+echo $a."tops </br>;
+}
+while
+$x=1;
+while ($x<=5)
+{
+    echo $x."<br>";
+    $x++;
+}
+
+//do...while loop
+do {
+echo $x."<br>";
+$x++;
+
+}while ($x<=0);
+
+?>
