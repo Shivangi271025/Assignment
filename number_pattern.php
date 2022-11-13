87103eb82b3b691b8b7d55d82f559ef6a33a43f5
Shivya <yadavshivangi366@gmail.com>
2022-11-13 20:13:47 +0530

    Create number_pattern.php

---
+++ number_pattern.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Number Pattern</title>
+    <style>
+        .fls {
+            width: 50%;
+            padding: 15px;
+            border-style: dotted;
+            border-color: red;
+            border-radius: 20px;
+
+        }
+
+        .fls1 {
+            width: 50%;
+            padding: 15px;
+            border-style: dotted;
+            border-color: green;
+            border-radius: 20px;
+        }
+
+        .fls2 {
+            width: 50%;
+            padding: 15px;
+            border-style: dotted;
+            border-color: blue;
+            border-radius: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <fieldset class="fls">
+        <legend><b>&nbsp; Question-6 &nbsp;</b></legend>
+        <p><b>Write a program to print the below format : </b></p>
+        <pre>5 9
+2 6 10
+3 7 11
+4 8 12</pre>
+    </fieldset>
+    </br>
+    <fieldset class="fls1">
+        <legend><b> &nbsp; Answer &nbsp;</b></legend>
+        <p>
+        echo "5 9&#60;br&#62;2 6 10&#60;br&#62;3 7 11&#60;br&#62;4 8 12";
+        </p>
+    </fieldset>
+    </br>
+    <fieldset class="fls2">
+        <legend><b>&nbsp; Output &nbsp;</b></legend>
+        <?php echo "5 9<br>2 6 10<br>3 7 11<br>4 8 12"; ?>
+    </fieldset>
+</body>
+
+</html>
